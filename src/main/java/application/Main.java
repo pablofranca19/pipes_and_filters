e3cad0eeb7b5b736dc5697c0d4f2c1301239ad6e
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-09-10 15:35:56 -0300

    main application

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -8,17 +8,21 @@ import filtering.process.Process;
 import model.Log;
 
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Main  {
     static void main(String[] args) {
 
-        String path = "src/logs/logs.txt";
-
         Process<String, List<String>> validator = new Validator();
         Process<List<String>, List<Log>> selector = new Selector();
         Process<List<Log>, List<Log>> anonymizer = new Anonymizer();
         Process<List<Log>, String> formatter = new Formatter();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the log file path: ");
+        String path = scanner.nextLine();
+        scanner.close();
 
         List<String> validLines = validator.process(path);
         List<Log> selectedLogs = selector.process(validLines);
@@ -26,6 +30,5 @@ public class Main  {
         String result = formatter.process(anonymizedLogs);
 
         System.out.println(result);
-
     }
 }
