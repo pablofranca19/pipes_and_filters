f64ccf9c7db78eb283d03ae914bae79e103f64e6
Pablo Melo Franca Filho <pablomelofrancafilho11@gmail.com>
2026-09-12 18:33:06 -0300

    main application - segregate main responsabilities

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -9,11 +9,25 @@ import model.Log;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Main  {
-    static void main(String[] args) {
 
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    // mutes an issue
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        run();
+        logger.log(Level.INFO, "Program initialized.");
+    }
+
+
+
+
+    private static void run () {
         Process<String, List<String>> validator = new Validator();
         Process<List<String>, List<Log>> selector = new Selector();
         Process<List<Log>, List<Log>> anonymizer = new Anonymizer();
@@ -24,11 +38,13 @@ public class Main  {
         String path = scanner.nextLine();
         scanner.close();
 
+        logger.log(Level.INFO, "File logging initialized");
+
         List<String> validLines = validator.process(path);
         List<Log> selectedLogs = selector.process(validLines);
         List<Log> anonymizedLogs = anonymizer.process(selectedLogs);
         String result = formatter.process(anonymizedLogs);
 
-        System.out.println(result);
+        logger.log(Level.INFO, result);
     }
 }
