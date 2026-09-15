69bdcd0108c5afc358e56069a89bfb45548f5ab0
Pablo Melo Franca Filho <pablomelofrancafilho11@gmail.com>
2026-09-15 11:59:10 -0300

    rename regex variable

--- src/test/java/input_test/ValidationTest.java
+++ src/test/java/input_test/ValidationTest.java
@@ -7,10 +7,13 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.awt.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 class ValidationTest {
 
@@ -18,10 +21,10 @@ class ValidationTest {
     @Test
     @DisplayName("Verify if the log matches the regex")
     void ifRegexMatchText () {
-        final String LOG_REGEX = "^" + IP_REGEX + "\\s-\\s-\\s" + "\\[" + DATE_REGEX + "\\]" + "\\s" + "\"" +  HTTP_REQUEST_REGEX + "\\s" + ENDPOINT_REGEX + "\\s" + HTTP_VERSION_REGEX + "\"" + "\\s" + STATUS_CODE_REGEX + "\\s" + BYTE_LENGTH_REGEX + "$";
+        final Pattern regexPattern = Pattern.compile("^" + IP_REGEX + "\\s-\\s-\\s" + "\\[" + DATE_REGEX + "\\]" + "\\s" + "\"" +  HTTP_REQUEST_REGEX + "\\s" + ENDPOINT_REGEX + "\\s" + HTTP_VERSION_REGEX + "\"" + "\\s" + STATUS_CODE_REGEX + "\\s" + BYTE_LENGTH_REGEX + "$");
         String log2 = "172.16.0.4 - - [10/Mar/2026:19:09:01] \"GET /admin HTTP/1.1\" 201 0";
 
-        assertTrue(log2.matches(LOG_REGEX), log2);
+        assertTrue(log2.matches(regexPattern.pattern()), log2);
     }
 
     @Test
@@ -38,5 +41,26 @@ class ValidationTest {
 
     }
 
+    @Test
+    @DisplayName("Checking regex logic with conditional")
+    void mustAddToAListOfStringIfMatches() {
 
+        List<String> validatedLogs = new ArrayList<>();
+
+        Path filePath = Path.of("src/logs/logs.txt");
+
+        try (BufferedReader bufferedReader = new BufferedReader(Files.newBufferedReader(filePath))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.matches(LOG_REGEX.pattern())) {
+                    validatedLogs.add(line);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertThat(validatedLogs).isNotEmpty();
+
+    }
 }
